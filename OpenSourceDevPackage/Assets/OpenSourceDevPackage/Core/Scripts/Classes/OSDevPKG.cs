1e31efc52c2d046480ec9e0aef8bd52622c35029
EmberFoxStudios <github@emberfox.studio>
2022-03-03 21:58:22 -0500

    Remove Unneeded Boiler Plate

--- OpenSourceDevPackage/Assets/OpenSourceDevPackage/Core/Scripts/Classes/OSDevPKG.cs
+++ OpenSourceDevPackage/Assets/OpenSourceDevPackage/Core/Scripts/Classes/OSDevPKG.cs
@@ -50,15 +50,5 @@ using UnityEngine;
 
 public class OSDevPKG
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
