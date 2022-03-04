77ae9361bafff65f7ff0bdfb26497ddb9db7982f
EmberFoxStudios <github@emberfox.studio>
2022-03-03 21:51:46 -0500

    Started Core Files

---
+++ OpenSourceDevPackage/Assets/OpenSourceDevPackage/Core/Scripts/Classes/OSDevPKG.cs
@@ -0,0 +1,64 @@
+﻿// License
+/*
+ MIT License
+
+Copyright (c) 2022 EmberFoxStudios
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software. Must also include a link to the project github repo https://github.com/emberfoxstudios/OpenSourceUnityDevPackage
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// Art
+/* 
+ _    _     _    _     _    _     _    _    _    _    _    _
+(.\\//.)   (.\\//.)   (.\\//.)   (.\\//.)  (.\\//.)  (.\\//.)
+ \ () /     \ () /     \ () /     \ () /    \ () /    \ () /
+ (_/\_)     (_/\_)     (_/\_)     (_/\_)    (_/\_)    (_/\_)
+
+ */
+
+// Script Description
+/*
+Script Description: This class is mandatroy to include with the package and contains all the nessceary functions to run all of this packages modular features.
+
+Please add your name or chosen online name and upt to 1 link in the below list if you have worked on this script.
+
+Contributors:
+
+Bella Weiss @ Ember Fox Studios
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSDevPKG
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
